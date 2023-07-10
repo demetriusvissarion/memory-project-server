4882cd54ba832b9a3da77c68b371681ad55e2be7
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-07-10 13:24:03 +0100

    connect express application

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from "./routes/posts.js";
+
 const app = express();
+
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -11,7 +15,6 @@ app.use(cors());
 
 const CONNECTION_URL =
   "mongodb+srv://memory:memory123@cluster0.klu4iyc.mongodb.net/?retryWrites=true&w=majority";
-
 const PORT = process.env.PORT || 5000;
 
 mongoose
@@ -23,5 +26,3 @@ mongoose
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
   .catch((error) => console.log(error.message));
-
-// mongoose.set("useFindAndModify", false);
